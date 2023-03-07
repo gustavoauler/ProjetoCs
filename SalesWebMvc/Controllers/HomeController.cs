3f7e6cb9aaacd760019ba165730b0a22117fdb22
gustavoauler <gustavooauler@gmail.com>
2023-03-07 15:28:08 -0300

    First Model-Controller-View - Department *ErrorViewModel

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Models.ViewModels;
+
 
 namespace SalesWebMvc.Controllers
 {
